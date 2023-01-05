9edfd9befefea9c5d024cc338b7272c904faaec2
Marcus Sjölin <msjolin@spotify.com>
2023-01-05 22:29:31 +0100

    Update setup.py
    updated version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('HISTORY.md') as history_file:
 
 setup_args = dict(
     name='swehockey_scraper',
-    version='0.4',
+    version='0.6',
     description='Functions to scrape ice hockey data and statistics from swehockey',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
